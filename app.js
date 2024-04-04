06b93286346eb9f633b5cb722e8c00236809eaa3
Giwa Halim <abdulalimgiwa@yahoo.com>
2024-04-04 16:50:45 +0100

    debugging

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.use('/', home)
 app.use('/api/user', user)
 app.use('/api/auth', auth)
 app.use('/api/report', report)
+// console.log(process.env.production)
 
 const port = process.env.PORT || 3005;
 app.listen(port, ()=> console.log(`Listening on port ${port}...`))
